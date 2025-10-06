44cb419bbfeb8e0f818b10bd59882b3b6957bb4b
TheMaxium69 <mxm.tournier@gmail.com>
2025-10-06 18:21:07 +0200

    Update login.php

--- panel/login.php
+++ panel/login.php
@@ -41,6 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://tyrolium.fr/Contenu/Image/Tyrolium Site.png" rel="shortcut icon">
     <title>Connexion - Card.Tyrolium</title>
     <style>
         :root {
